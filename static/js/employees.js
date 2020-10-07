24dd96458d1b909c43927e263b39c1098376a5f7
Robbm <robert.brindley.morgan+github@gmail.com>
2020-10-07 23:48:13 +0100

    Added static functions to employees class:

---
+++ static/js/employees.js
@@ -0,0 +1,100 @@
+class Employees {
+  constructor(data) {
+    this.employees = data;
+  }
+
+  get getEmployees() {
+    return this.employees;
+  }
+
+  set setEmployees(employees) {
+    this.employees = employees;
+  }
+
+  static async fetchAllEmployees() {
+    let response = await fetch("/static/php/personnel/getAllPersonnel.php");
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  }
+
+  static async fetchEmployeeByID(id) {
+    var formData = new FormData();
+    formData.append("id", id);
+    let response = await fetch("/static/php/personnel/getPersonnelByID.php", {
+      method: "POST",
+      body: formData,
+    });
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  }
+
+  static async deleteEmployee(id) {
+    var formData = new FormData();
+    formData.append("id", id);
+    let response = await fetch(
+      "/static/php/personnel/deletePersonnelByID.php",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  }
+
+  static async insertEmployee(
+    firstName,
+    lastName,
+    email,
+    jobTitle,
+    locationID,
+    departmentID
+  ) {
+    var formData = new FormData();
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("email", email);
+    formData.append("jobTitle", jobTitle);
+    formData.append("locationID", locationID);
+    formData.append("departmentID", departmentID);
+    let response = await fetch("/static/php/personnel/insertPersonnel.php", {
+      method: "POST",
+      body: formData,
+    });
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  }
+
+  static async updateEmployeeByID(
+    id,
+    firstName,
+    lastName,
+    email,
+    jobTitle,
+    locationID,
+    departmentID
+  ) {
+    var formData = new FormData();
+    formData.append("id", id);
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("email", email);
+    formData.append("jobTitle", jobTitle);
+    formData.append("locationID", locationID);
+    formData.append("departmentID", departmentID);
+    let response = await fetch(
+      "/static/php/personnel/updatePersonnelByID.php",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    let responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+  }
+}
