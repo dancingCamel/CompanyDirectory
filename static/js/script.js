e422853bbd47bdc89bc0e966172768fc7f41bc85
Robbm <robert.brindley.morgan+github@gmail.com>
2020-10-15 00:51:08 +0100

    Adding filter functionality

--- static/js/script.js
+++ static/js/script.js
@@ -46,27 +46,53 @@ $(document).ready(function () {
         .columns([1, 3, 5, 6])
         .every(function () {
           var column = this;
+          var columnIndex = column.index();
           var select = $(
             '<select><option value="">' +
               column.header().innerHTML +
               "</option></select>"
-          )
-            .prependTo($(column.footer()).empty())
-            .on("change", function () {
-              var val = $.fn.dataTable.util.escapeRegex($(this).val());
+          );
+          // add to row of other table here
+          // .prependTo(employeesFilterTable.columns([columnIndex]).cells());
+          // this on change function needs to be a seperate function, invoked in the onChange handler of html in render function
+          // .on("change", function () {
+          //   var val = $.fn.dataTable.util.escapeRegex($(this).val());
+          //   // column.search(val ? "^" + val + "$" : "", true, false).draw();
 
-              column.search(val ? "^" + val + "$" : "", true, false).draw();
-            });
+          //   employeesTable
+          //     .columns([columnIndex])
+          //     .search(val ? "^" + val + "$" : "", true, false)
+          //     .draw();
+          // });
 
           column
             .data()
             .unique()
             .sort()
             .each(function (d, j) {
-              select.append('<option value="' + d + '">' + d + "</option>");
+              // here we need to select the cell data in other table
+              let $option = $(document.createElement("option"));
+              $option.val(d);
+              $option.html(d);
+              console.log(employeesFilterTable.cell(1, columnIndex));
+              $(".employeeFilterSelect")[Math.floor(columnIndex / 2)].append(
+                $option
+              );
+              // need to draw the table every time we append data? can chain it?
+              employeesFilterTable.draw();
             });
         });
     },
+  });
+
+  $(".employeeFilterSelect").on("change", function () {
+    var val = $.fn.dataTable.util.escapeRegex($(this).val());
+    // column.search(val ? "^" + val + "$" : "", true, false).draw();
+
+    employeesTable
+      .columns([columnIndex])
+      .search(val ? "^" + val + "$" : "", true, false)
+      .draw();
   });
 
   $("#employeesTable tbody").on("click", "tr .edit", function () {
@@ -83,6 +109,96 @@ $(document).ready(function () {
     } else {
       showError(response.status.description);
     }
+  });
+
+  // employees filter table
+  // the data in this table overwrites anything added in the last table.
+  // don't make this a data table? just manually put selects in the main page and populate from employeesTable data
+  // leave the on change function
+  const employeesFilterTable = $("#employeesFilterTable").DataTable({
+    paging: false,
+    ordering: false,
+    searching: false,
+    info: false,
+    responsive: true,
+    columnDefs: [
+      {
+        render: function (data, type, row) {
+          return (
+            '<select class="employeeFilterSelect"><option value="">' +
+            // grab name from other table
+            employeesTable.column([1]).header().innerHTML +
+            "</option></select>"
+          );
+        },
+        targets: 0,
+      },
+      {
+        render: function (data, type, row) {
+          return (
+            '<select class="employeeFilterSelect"><option value="">' +
+            // grab name from other table
+            employeesTable.column([3]).header().innerHTML +
+            "</option></select>"
+          );
+        },
+        targets: 2,
+      },
+      {
+        render: function (data, type, row) {
+          return (
+            '<select class="employeeFilterSelect"><option value="">' +
+            // grab name from other table
+            employeesTable.column([4]).header().innerHTML +
+            "</option></select>"
+          );
+        },
+        targets: 4,
+      },
+      {
+        render: function (data, type, row) {
+          return (
+            '<select class="employeeFilterSelect"><option value="">' +
+            // grab name from other table
+            employeesTable.column([5]).header().innerHTML +
+            "</option></select>"
+          );
+        },
+        targets: 5,
+      },
+    ],
+    // working here
+    // initComplete: function () {
+    //   this.api()
+    //     .columns()
+    //     .every(function () {
+    //       var column = this;
+    //       var columnIndex = column.index();
+    //       console.log(columnIndex);
+    //       var select = $(
+    //         '<select><option value="">' +
+    //           column.header().innerHTML +
+    //           "</option></select>"
+    //       )
+    //         .prependTo($(column.footer()).empty())
+    //         .on("change", function () {
+    //           var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+    //           column.search(val ? "^" + val + "$" : "", true, false).draw();
+    //         });
+
+    //       // this function is running before getting data from ajax in other table.
+    //       employeesTable
+    //         .columns([columnIndex])
+    //         .data()
+    //         .unique()
+    //         .sort()
+    //         .each(function (d, j) {
+    //           console.log(d);
+    //           // select.append('<option value="' + d + '">' + d + "</option>");
+    //         });
+    //     });
+    // },
   });
 
   const departmentsTable = $("#departmentsTable").DataTable({
