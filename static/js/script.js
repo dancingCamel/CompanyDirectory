0f0b95f7cc605dd658947dad090fa993431adf7a
Robbm <robert.brindley.morgan+github@gmail.com>
2020-10-19 15:10:07 +0100

    extracted datatable setup to util functions

--- static/js/script.js
+++ static/js/script.js
@@ -19,61 +19,7 @@ $(document).ready(function () {
     $(".navbar-collapse").collapse("hide");
   });
 
-  // set up data tables
-  const employeesTable = $("#employeesTable").DataTable({
-    responsive: true,
-    autoWidth: false,
-    ajax: { url: "/static/php/personnel/getAllPersonnel.php", dataSrc: "data" },
-    columns: [
-      { data: "id", visible: false },
-      { data: "lastName" },
-      { data: "firstName" },
-      { data: "jobTitle" },
-      { data: "email" },
-      { data: "department" },
-      { data: "location" },
-      {
-        data: null,
-        orderable: false,
-        // width: 180,
-        render: function (data, type, row, meta) {
-          return (
-            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
-            row.id +
-            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
-            row.id +
-            '><i class="fas fa-trash-alt"></i></button></div>'
-          );
-        },
-      },
-    ],
-    initComplete: function () {
-      this.api()
-        .columns([1, 3, 5, 6])
-        .every(function () {
-          var column = this;
-          var columnIndex = column.index();
-          var select = $(
-            '<select><option value="">' +
-              column.header().innerHTML +
-              "</option></select>"
-          );
-
-          column
-            .data()
-            .unique()
-            .sort()
-            .each(function (d, j) {
-              let $option = $(document.createElement("option"));
-              $option.val(d);
-              $option.html(d);
-              $($(".employeeFilterSelect")[Math.floor(columnIndex / 2)]).append(
-                $option
-              );
-            });
-        });
-    },
-  });
+  // set up data table functionality
 
   $(".employeeFilterSelect").on("change", function () {
     console.log(this);
@@ -112,56 +58,6 @@ $(document).ready(function () {
     }
   });
 
-  const departmentsTable = $("#departmentsTable").DataTable({
-    responsive: true,
-    autoWidth: false,
-    ajax: { url: "/static/php/dept/getAllDepartments.php", dataSrc: "data" },
-    columns: [
-      { data: "id", visible: false },
-      { data: "name" },
-      { data: "locationID", visible: false },
-      { data: "location" },
-      {
-        data: null,
-        orderable: false,
-        width: "180px",
-        render: function (data, type, row, meta) {
-          return (
-            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
-            row.id +
-            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
-            row.id +
-            '><i class="fas fa-trash-alt"></i></button></div>'
-          );
-        },
-      },
-    ],
-    initComplete: function () {
-      this.api()
-        .columns([3])
-        .every(function () {
-          var column = this;
-          var columnIndex = column.index();
-          var select = $(
-            '<select><option value="">' +
-              column.header().innerHTML +
-              "</option></select>"
-          );
-
-          column
-            .data()
-            .unique()
-            .sort()
-            .each(function (d, j) {
-              let $option = $(document.createElement("option"));
-              $option.val(d);
-              $option.html(d);
-              $($(".departmentFilterSelect")[0]).append($option);
-            });
-        });
-    },
-  });
-
   $(".departmentFilterSelect").on("change", function () {
     // console.log(this);
     var val = $.fn.dataTable.util.escapeRegex($(this).val());
@@ -197,54 +93,6 @@ $(document).ready(function () {
         showError(response.status.description);
       }
     }
-  });
-
-  const locationsTable = $("#locationsTable").DataTable({
-    responsive: true,
-    autoWidth: false,
-    ajax: { url: "/static/php/location/getAllLocations.php", dataSrc: "data" },
-    columns: [
-      { data: "id", visible: false },
-      { data: "name" },
-      {
-        data: null,
-        orderable: false,
-        width: "180px",
-        render: function (data, type, row, meta) {
-          return (
-            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
-            row.id +
-            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
-            row.id +
-            '><i class="fas fa-trash-alt"></i></button></div>'
-          );
-        },
-      },
-    ],
-    initComplete: function () {
-      this.api()
-        .columns([1])
-        .every(function () {
-          var column = this;
-          var columnIndex = column.index();
-          var select = $(
-            '<select><option value="">' +
-              column.header().innerHTML +
-              "</option></select>"
-          );
-
-          column
-            .data()
-            .unique()
-            .sort()
-            .each(function (d, j) {
-              let $option = $(document.createElement("option"));
-              $option.val(d);
-              $option.html(d);
-              $($(".locationFilterSelect")[0]).append($option);
-            });
-        });
-    },
   });
 
   $("#locationsTable tbody").on("click", "tr .edit", function () {
@@ -394,7 +242,7 @@ $("#loginBtn").click(function (e) {
 
 const testFuncs = () => {
   // showEmployeesPage();
-  // showDepartmentsPage();
+  showDepartmentsPage();
   // showLocationsPage();
   // showCreateLocationPage();
   // showCreateDepartmentPage();
@@ -402,5 +250,5 @@ const testFuncs = () => {
   // showEditEmployeePage(exampleID);
   // showEditDepartmentPage(exampleID);
   // showEditLocationPage(exampleID);
-  showLoginPage();
+  // showLoginPage();
 };
--- static/js/utils.js
+++ static/js/utils.js
@@ -139,7 +139,11 @@ const showEmployeesPage = () => {
   hideAllPages();
   showNav();
   activeNavEmployees();
-  // $("#employeesTable").DataTable().columns.adjust().draw();
+  if (!populatedEmployeesTable) {
+    // pass jwt here
+    populateEmployeesTable();
+    populatedEmployeesTable = true;
+  }
   $("#employeesPage").show();
   $("#employeesTable").DataTable().columns.adjust().draw();
 };
@@ -148,6 +152,11 @@ const showDepartmentsPage = () => {
   hideAllPages();
   showNav();
   activeNavDepts();
+  if (!populatedDepartmentsTable) {
+    // pass jwt here
+    populateDepartmentsTable();
+    populatedDepartmentsTable = true;
+  }
   $("#departmentsPage").show();
   $("#departmentsTable").DataTable().columns.adjust().draw();
 };
@@ -156,7 +165,11 @@ const showLocationsPage = () => {
   hideAllPages();
   showNav();
   activeNavLocations();
-  // repopulate locations table here
+  if (!populatedLocationsTable) {
+    // pass jwt here
+    populateLocationsTable();
+    populatedLocationsTable = true;
+  }
   $("#locationsPage").show();
   $("#locationsTable").DataTable().columns.adjust().draw();
 };
@@ -390,4 +403,170 @@ const loaderWrapper = (wrapped) => {
   };
   setTimeout(hideLoader, 500);
   return result;
+};
+
+// set up data tables
+let employeesTable;
+let populatedEmployeesTable = false;
+const populateEmployeesTable = () => {
+  employeesTable = $("#employeesTable").DataTable({
+    responsive: true,
+    autoWidth: false,
+    ajax: { url: "/static/php/personnel/getAllPersonnel.php", dataSrc: "data" },
+    columns: [
+      { data: "id", visible: false },
+      { data: "lastName" },
+      { data: "firstName" },
+      { data: "jobTitle" },
+      { data: "email" },
+      { data: "department" },
+      { data: "location" },
+      {
+        data: null,
+        orderable: false,
+        // width: 180,
+        render: function (data, type, row, meta) {
+          return (
+            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
+            row.id +
+            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
+            row.id +
+            '><i class="fas fa-trash-alt"></i></button></div>'
+          );
+        },
+      },
+    ],
+    initComplete: function () {
+      this.api()
+        .columns([1, 3, 5, 6])
+        .every(function () {
+          var column = this;
+          var columnIndex = column.index();
+          var select = $(
+            '<select><option value="">' +
+              column.header().innerHTML +
+              "</option></select>"
+          );
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              let $option = $(document.createElement("option"));
+              $option.val(d);
+              $option.html(d);
+              $($(".employeeFilterSelect")[Math.floor(columnIndex / 2)]).append(
+                $option
+              );
+            });
+        });
+    },
+  });
+};
+
+let departmentsTable;
+let populatedDepartmentsTable = false;
+const populateDepartmentsTable = () => {
+  departmentsTable = $("#departmentsTable").DataTable({
+    responsive: true,
+    autoWidth: false,
+    ajax: { url: "/static/php/dept/getAllDepartments.php", dataSrc: "data" },
+    columns: [
+      { data: "id", visible: false },
+      { data: "name" },
+      { data: "locationID", visible: false },
+      { data: "location" },
+      {
+        data: null,
+        orderable: false,
+        width: "180px",
+        render: function (data, type, row, meta) {
+          return (
+            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
+            row.id +
+            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
+            row.id +
+            '><i class="fas fa-trash-alt"></i></button></div>'
+          );
+        },
+      },
+    ],
+    initComplete: function () {
+      this.api()
+        .columns([3])
+        .every(function () {
+          var column = this;
+          var columnIndex = column.index();
+          var select = $(
+            '<select><option value="">' +
+              column.header().innerHTML +
+              "</option></select>"
+          );
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              let $option = $(document.createElement("option"));
+              $option.val(d);
+              $option.html(d);
+              $($(".departmentFilterSelect")[0]).append($option);
+            });
+        });
+    },
+  });
+};
+
+let locationsTable;
+let populatedLocationsTable = false;
+const populateLocationsTable = () => {
+  locationsTable = $("#locationsTable").DataTable({
+    responsive: true,
+    autoWidth: false,
+    ajax: { url: "/static/php/location/getAllLocations.php", dataSrc: "data" },
+    columns: [
+      { data: "id", visible: false },
+      { data: "name" },
+      {
+        data: null,
+        orderable: false,
+        width: "180px",
+        render: function (data, type, row, meta) {
+          return (
+            '<div><button class="btn btn-primary mr-2 table-button edit" data-row=' +
+            row.id +
+            '><i class="fas fa-edit"></i></button><button class="btn btn-danger table-button delete" data-row=' +
+            row.id +
+            '><i class="fas fa-trash-alt"></i></button></div>'
+          );
+        },
+      },
+    ],
+    initComplete: function () {
+      this.api()
+        .columns([1])
+        .every(function () {
+          var column = this;
+          var columnIndex = column.index();
+          var select = $(
+            '<select><option value="">' +
+              column.header().innerHTML +
+              "</option></select>"
+          );
+
+          column
+            .data()
+            .unique()
+            .sort()
+            .each(function (d, j) {
+              let $option = $(document.createElement("option"));
+              $option.val(d);
+              $option.html(d);
+              $($(".locationFilterSelect")[0]).append($option);
+            });
+        });
+    },
+  });
 };
